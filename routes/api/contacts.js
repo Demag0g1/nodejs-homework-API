fd234787e70c3f908647ecf9e9b90dc7decf3f6c
Demag0g1 <konievanatol@gmail.com>
2023-07-17 16:14:13 +0300

    first init

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,6 +1,17 @@
-const express = require('express')
+const express = require('express');
+// const mongoose=require ('mongoose');
+
+// mongoose.connect (DB_HOST);
+
+// const DB_HOST="mongodb+srv://demagog:10101982@contacts.ttvf3bm.mongodb.net/contacts?retryWrites=true&w=majority";
+
+
+// mongoose.connect (DB_HOST)
+//   .then(()=>console.log ("Database connection successful"))
+//   .catch(error=>console.log (error.message) )
 
 const router = express.Router()
+
 
 router.get('/', async (req, res, next) => {
   res.json({ message: 'template message' })
