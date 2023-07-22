04acd87de7f25e9c4e0fd167fcbf742719dad1cc
Demag0g1 <konievanatol@gmail.com>
2023-07-22 17:18:30 +0300

    any commit

--- server.js
+++ server.js
@@ -1,59 +1,66 @@
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-
-const express = require('express');
-const logger = require('morgan');
-const cors = require('cors');
-const mongoose = require('mongoose');
-
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const contactsRouter = require('./routes/api');
-app.use('/api/contacts', contactsRouter);
+const contactsRouter = require("./routes/api");
+app.use(
+  "/api/contacts",
+  contactsRouter
+);
 
 app.use((_, res, __) => {
   res.status(404).json({
-    status: 'error',
+    status: "error",
     code: 404,
-    message: 'Use api on routes: /api/index',
-    data: 'Not found',
-  })
-})
+    message:
+      "Use api on routes: /api/index",
+    data: "Not found",
+  });
+});
 app.use((err, _, res, __) => {
-  console.log(err.stack)
+  console.log(err.stack);
   res.status(500).json({
-    status: 'fail',
+    status: "fail",
     code: 500,
     message: err.message,
-    data: 'Internal Server Error',
-  })
-})
+    data: "Internal Server Error",
+  });
+});
 
-
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const formatsLogger =
+  app.get("env") === "development"
+    ? "dev"
+    : "short";
 app.use(logger(formatsLogger));
 
-const PORT =process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 const dbHost = process.env.DB_HOST;
 
-mongoose.connect((dbHost), {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-})
+mongoose
+  .connect(dbHost, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     app.listen(PORT, function () {
-      console.log(`Database connection successful. Server running. Use our API on port: ${PORT}`)
-    })
+      console.log(
+        `Database connection successful. Server running. Use our API on port: ${PORT}`
+      );
+    });
   })
-  .catch((err) =>
-    console.log(`Server not running. Error message: ${err.message}`),
+  .catch(
+    (err) =>
+      console.log(
+        `Server not running. Error message: ${err.message}`
+      ),
     process.exit(1)
   );
-
-
-
