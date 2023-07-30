c76cd103441750fba0c6961baa77663208b2d331
Demag0g1 <konievanatol@gmail.com>
2023-07-30 10:31:52 +0300

    update auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,9 +3,16 @@ const User = require("../service");
 
 const secretKey = "Demag0g";
 
-const authenticateToken = async (req, res, next) => {
+const authenticateToken = async (
+  req,
+  res,
+  next
+) => {
   try {
-    const token = req.headers.authorization?.split(" ")[1];
+    const token =
+      req.headers.authorization?.split(
+        " "
+      )[1];
 
     if (!token) {
       return res.status(401).json({
@@ -13,9 +20,14 @@ const authenticateToken = async (req, res, next) => {
       });
     }
 
-    const decodedToken = jwt.verify(token, secretKey);
+    const decodedToken = jwt.verify(
+      token,
+      secretKey
+    );
 
-    const user = await User.findById(decodedToken.id);
+    const user = await User.findById(
+      decodedToken.id
+    );
 
     if (!user || user.token !== token) {
       return res.status(401).json({
@@ -30,6 +42,8 @@ const authenticateToken = async (req, res, next) => {
       message: "Not authorized",
     });
   }
+
+  console.log("authenticateToken");
 };
 
 module.exports = authenticateToken;
